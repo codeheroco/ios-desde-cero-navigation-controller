eadb8f6ba3951874c4cdfc50e5813b867cdf7414
Oscar Vicente González Greco <oscar@Gonzalezs-iMac.local>
2013-07-21 21:02:32 -0430

    Initial Commit

---
+++ Navigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Navigation
+//
+//  Created by Oscar Vicente González Greco on 21/7/13.
+//  Copyright (c) 2013 Codehero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
